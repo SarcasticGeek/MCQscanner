877e8befd34e16148b2b0e3b64cb3e3ddf5dfad5
Mohamed Essam Fathalla <m_essamfathalla@yahoo.com>
2016-05-19 09:35:34 +0200

    add wrapping

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import  math
 #Orginal image
 imgdir = 'testcase.jpg'
 Orimg = cv2.imread(imgdir)
+
+
 small = cv2.resize(Orimg, (0,0), fx=0.4, fy=0.4)
+
 small2 = cv2.resize(Orimg, (0,0), fx=0.4, fy=0.4)
+small3 = cv2.resize(Orimg, (0,0), fx=0.4, fy=0.4)
+
 greysmall = cv2.cvtColor(small,cv2.COLOR_RGB2GRAY)
 aftergauss = cv2.GaussianBlur(greysmall,(11,11),0)
 #ret,afterthreshold = cv2.threshold(aftergauss, 10, 255, cv2.THRESH_BINARY)
@@ -15,6 +20,22 @@ aftergauss = cv2.GaussianBlur(greysmall,(11,11),0)
 afterthreshold = cv2.adaptiveThreshold(aftergauss,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C , cv2.THRESH_BINARY,75,10);
 afterbitwise = cv2.bitwise_not( afterthreshold)
 cv2.imshow("kk",afterbitwise)
+#Rotate image
+xyofBigCirles = []
+img3,contourssss, hierarchys = cv2.findContours(afterbitwise,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for cont in contourssss:
+    (x,y),radius = cv2.minEnclosingCircle(cont)
+    if int(radius) == 16 and int(y) > (3 * small3.shape[1])/4:
+        cv2.circle(small3,(int(x),int(y)),int(radius),(0,255,255),3)
+        xyofBigCirles.append([int(x),int(y)])
+angle = int(math.atan((xyofBigCirles[0][1]-xyofBigCirles[1][1])/(xyofBigCirles[1][0]-xyofBigCirles[0][0]))*180/math.pi)
+print 'angle' , angle
+r = cv2.getRotationMatrix2D((small3.shape[0]/2.,small3.shape[0]/2.),angle,1.0)
+newimage = cv2.warpAffine(small3,r,(small3.shape[0],small3.shape[0]))
+cv2.imshow('newimage',newimage)
+
+cv2.imshow("real on",small3)
+
 
 torgb = cv2.cvtColor(afterbitwise,cv2.COLOR_GRAY2BGR)
 borders = cv2.HoughLinesP(afterbitwise,1, np.pi / 180 ,80,400,10).tolist()
